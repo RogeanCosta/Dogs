80d069abb59803d23007fdd136d501bfbc5f9870
Rogean Costa <rogean.ifce@gmail.com>
2024-11-05 12:14:05 -0300

    Página de estatísticas criadacom auxílio da biblioteca Victory para geração de diferentes tipos de gráficos.

--- src/User/UserStats.jsx
+++ src/User/UserStats.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Head from '../Components/Helper/Head';
+import useFetch from '../Hooks/useFetch';
+import { STATS_GET } from '../Api';
+import Loading from '../Components/Helper/Loading';
+import Error from '../Components/Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  }
+  return null;
 };
 
 export default UserStats;
