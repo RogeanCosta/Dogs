c604bbee8610242f4e075101c02bdad2c455f2ca
Rogean Costa <rogean.ifce@gmail.com>
2024-11-05 09:52:00 -0300

    Página de perfil do usuário, somente com suas postagens, criada.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { UserStorage } from './UserContext';
 import User from './User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './User/UserProfile';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
